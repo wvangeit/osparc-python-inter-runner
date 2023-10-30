8a109f5142b12a25ba8e5655913afeb2da53dbd1
Werner Van Geit <werner.vangeit@epfl.ch>
2023-10-30 15:17:41 +0100

    Run main in background

--- docker/runner.py
+++ docker/runner.py
@@ -28,9 +28,9 @@ def _find_user_code_entrypoint(code_dir: Path) -> Path:
         if not code_files:
             raise ValueError("No main.py found in user-provided directory")
         if len(code_files) > 1:
-            raise ValueError(f"Multiple main python files found"
+            raise ValueError("Multiple main python files found"
                              " in user-provided directory,"
-                             "I don't know which one to run: {code_files}")
+                             f"I don't know which one to run: {code_files}")
 
     main_py = code_files[0]
     logger.info("Found %s as python script", main_py)
@@ -41,9 +41,9 @@ def _ensure_pip_requirements(code_dir: Path) -> Path:
     logger.info("Searching for requirements file ...")
     requirements = list(code_dir.rglob("requirements.txt"))
     if len(requirements) > 1:
-        raise ValueError(f"Multiple requirements found, "
+        raise ValueError("Multiple requirements found, "
                          "don't know which one to pick"
-                         ": {requirements}")
+                         f": {requirements}")
     elif len(requirements) == 0:
         return None
     else:
@@ -91,7 +91,7 @@ def setup():
         if requirements_txt else '',
         f'echo "Executing code {user_code_entrypoint.name}..."',
         f'cd "{user_code_entrypoint.parents[0]}"',
-        f'"{venv_dir}/bin/python3" "{user_code_entrypoint}"',
+        f'"{venv_dir}/bin/python3" "{user_code_entrypoint}" &',
         'echo "DONE ..."',]
     main_script_path = Path("main.sh")
     main_script_path.write_text("\n".join(script), encoding="utf-8")
